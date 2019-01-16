1fa7d09330aa0ad88ac0268bfa6918717b2749e9
Hassan Alamri <alamri.cs@gmail.com>
2019-01-15 23:46:10 -0500

    Add Dockerfile and fix path
    1. Add docker file to user api
    2. update user.js path in db.js
    3. update description in package.json

--- User_API/model/db.js
+++ User_API/model/db.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/User')
+mongoose.connect('mongodb://testdb:27017/retail_db')
   .then(() =>  console.log('connection succesful'))
   .catch((err) => console.error(err));
+
+console.log('So far so goooooooooooooooood!!!!!!!!!!!!!!');
 
 require('./user');
